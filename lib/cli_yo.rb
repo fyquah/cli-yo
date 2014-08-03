7d24190884dad99d4b4fa8b692b248f09e303cbe
fyquah95 <quah.fy95@gmail.com>
2014-08-03 15:38:34 +0800

    changed into plural usernames

--- lib/cli_yo.rb
+++ lib/cli_yo.rb
@@ -3,21 +3,18 @@ require "json"
 YO_URL = "http://api.justyo.co/yo/"
 
 module Cli_Yo
-	@short_cut_mapping = {
-		s: :silent,
-		a: :api_token,
-		c: :count , 
-		i: :interval
-	}
-	@all_properties = [:silent , :count , :username , :api_token , :interval];
-	@numeric_properties = [:count , :interval];
-	@mandatory_properties = [:username , :api_token]
+
+	@all_properties = [:silent , :times , :usernames , :api_token , :interval];
+	@numeric_properties = [:times , :interval];
+	@mandatory_properties = [:usernames , :api_token]
 
 	class << self
 		attr_reader :short_cut_mapping , :all_properties , :numeric_properties , :mandatory_properties
 	end
 
 	class Yo_Error < Exception
+		attr_reader :error , :code
+
 		def initialize error , code = nil
 			@error = error
 			@code = code
@@ -35,10 +32,12 @@ module Cli_Yo
 	class Yo_Arguments
 		def initialize arguments = Hash.new
 			@arguments = Hash.new
+
 			Cli_Yo.all_properties.each do |prop|
 				@arguments[prop] = arguments[prop]
 			end
-			@arguments[:count] ||= 1
+
+			@arguments[:times] ||= 1
 			@arguments[:api_token] ||= `echo $YO_TOKEN`.chomp
 			@arguments[:interval] ||= 1
 		end
@@ -76,28 +75,45 @@ module Cli_Yo
 			end
 			"#{n}-#{suffix}"
 		end
+
+		def self.beautiful_sentence arr
+			return nil if arr.size == 0
+			return arr[0] if arr.size == 1
+
+			str = ""
+			for i in 0...(arr.size - 1) do
+				str += ", " unless i == 0
+				str += "#{arr[i]}"
+			end
+			str = "#{str} and #{arr.last}"
+		end
 	end
 
 	def self.yo! arguments
-		raise Yo_Error.new unless arguments.class == Hash || arguments.class == Yo_Arguments
+
+		raise Yo_Error.new "invalid arguments class provided!" unless arguments.class == Hash || arguments.class == Yo_Arguments
 		arguments = Yo_Arguments.new arguments if arguments.class == Hash
+
 		begin
 
+			raise Yo_Error.new "Noone to yo!" if arguments.usernames.size == 0
 			Cli_Yo.mandatory_properties.each do |prop|
 				raise Yo_Error.new "Missing mandatory argument #{prop}" unless arguments.property_is_set? (prop)
 			end
 
-			puts "Going to start yo-ing #{arguments.username}"
-			Process.daemon if arguments.silent
-			counter = arguments.count
+			puts "Going to start yo-ing #{Helper::beautiful_sentence(arguments.usernames)}"
+			puts "Pid of current Process is #{Process.pid} "
+
+			Process.daemon('./') if arguments.silent
+			counter = arguments.times
 
 			until counter <= 0 do
-				result = RestClient.post YO_URL , {username: arguments.username , api_token: arguments.api_token}
-				result = JSON.parse result , symbolize_names: true
-
-				raise Yo_Error.new(result[:error] , result[:code]) if result[:code] || result[:error]
-
-				puts "\nYo-ed #{arguments.username} ! This is the #{Helper::consecutive_counter (arguments.count - counter + 1)} Yo!" unless arguments.silent
+				arguments.usernames.each do |username|
+					result = RestClient.post YO_URL , {username: username , api_token: arguments.api_token}
+					result = JSON.parse result , symbolize_names: true
+					raise Yo_Error.new(result[:error] , result[:code]) if result[:code] || result[:error]
+				end
+				puts "Yo-ed #{Helper::beautiful_sentence(arguments.usernames)} ! This is the #{Helper::consecutive_counter (arguments.times - counter + 1)} Yo!!" unless arguments.silent
 				counter -= 1
 				break if counter <= 0
 				sleep arguments.interval * 60  #Yo allows at most 1 minutes once!
@@ -105,14 +121,11 @@ module Cli_Yo
 
 			unless arguments.silent
 				puts "\nCompleted your Yo-es with ultimate awesomeness!"
-				puts "Add the silent option if you don't want this message to pop up!"
+				puts "Add the silent option if you don't want these messages to pop up!"
 			end
 
 		rescue Yo_Error => err
 			puts err
 		end
 	end
-
-
 end
-
